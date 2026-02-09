e810cc846b3da0a52f3cd0bff8df2470584eb0ed
PochaDongDong <lim249670@gmail.com>
2026-02-09 20:59:36 +0900

    마이 페이지 만듬 1. 계정 정보 수정 가능(이미지 업로드는 아직 안됨) 2. 스터디 참가 버튼'만' 만듬

--- studygroup/src/main/java/com/example/studygroup/domain/User.java
+++ studygroup/src/main/java/com/example/studygroup/domain/User.java
@@ -19,15 +19,30 @@ public class User {
     private String name;
     private String email;
     private String phoneNumber;
-    private LocalDate birthDate; // DB의 DATE 타입과 매핑
+    private LocalDate birthDate;
+
+    private String profileImage; // 프로필 이미지 경로
 
     @Builder
-    public User(String username, String password, String name, String email, String phoneNumber, LocalDate birthDate) {
+    public User(String username, String password, String name, String email, String phoneNumber, LocalDate birthDate, String profileImage) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
+        this.profileImage = profileImage;
+    }
+
+    // 프로필 이미지 업데이트
+    public void updateProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    // 회원 정보 수정
+    public void updateInfo(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 }
