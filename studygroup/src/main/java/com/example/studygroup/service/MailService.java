c151c77cc1833aa3f32e70d2863b44cfe66de0e5
baektom <baektom77@gmail.com>
2026-02-10 18:04:23 +0900

    메일 발송 안되는 이슈 해결

--- studygroup/src/main/java/com/example/studygroup/service/MailService.java
+++ studygroup/src/main/java/com/example/studygroup/service/MailService.java
@@ -4,12 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
     private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
 
     // 1. 6자리 인증번호 생성 (image_cffda5.png의 line 21 에러 해결)
     public String createCode() {
@@ -20,11 +25,12 @@ public class MailService {
     public void sendEmail(String toEmail, String code) {
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom("loveyoo2015@gmail.com");
+        message.setFrom(fromEmail);   // ← 여기 변경
 
         message.setTo(toEmail);
         message.setSubject("[STUDY MATE] 회원가입 인증번호입니다.");
         message.setText("인증번호는 [" + code + "] 입니다. 3분 이내에 입력해주세요.");
         mailSender.send(message);
     }
+
 }
